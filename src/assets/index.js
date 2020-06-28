359299598ac9f3d4dc5c57165cebdb630e92e15b
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-06-28 21:12:33 +0100

    chore: add image assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,19 @@
 import headerBckgrnd from './black-white-me.jpg';
 import meFaded from './faded-me.jpg';
+import mcompetitionDetail from './m1.png';
+import mcompetitionDetail2 from './m2.png';
+import mpostview from './m3.png';
+import mcategories from './m4.png';
+import mcompetitions from './m5.png';
+import msplashscreen from './m6.png';
 
- export default {
+export default {
   headerBckgrnd,
   meFaded,
+  mcompetitionDetail,
+  mcompetitionDetail2,
+  mpostview,
+  mcategories,
+  mcompetitions,
+  msplashscreen,
 };
