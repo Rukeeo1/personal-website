f2af1d58a515bc09ee4eafef102b85176d353feb
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-06-28 19:40:05 +0100

    chore: add header wrapper

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import images from 'assets/index';
+import './index.scss';
+console.log(images, 'this is rukee');
+
+export default function Header() {
+  return (
+    <div className="header-wrapper">
+      {/* <img src={images.meFaded} /> */}
+      <div></div>
+    </div>
+  );
+}
