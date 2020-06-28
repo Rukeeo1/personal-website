031ebfa8770c42e9ca777f27ae643494d068211f
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-06-28 19:37:02 +0100

    chore: add dummy data to workexperience

---
+++ src/data/workexperience.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    title: 'Maudition',
+    info: 'A mobile platform for hosting auditions',
+    rolePlayed: '',
+    utilised: [
+      'React Native',
+      'Expo',
+      'React Navigation',
+      'Redux',
+      'NodeJS',
+      'Sequelize',
+      'Postgresql',
+      'Jest',
+    ],
+  },
+  {
+    title: 'Peerless',
+    rolePlayed: '',
+    info:
+      'A learning management solution for companies and educational institutions toeducate students, staff and conduct assessments.',
+    utilised: [
+      'React Native',
+      'Expo',
+      'React Navigation',
+      'Redux',
+      'NodeJS',
+      'Sequelize',
+      'Postgresql',
+      'Jest',
+    ],
+  },
+  {
+    title: 'Outwork',
+    rolePlayed: '',
+    info:
+      'A learning management solution for companies and educational institutions toeducate students, staff and conduct assessments.',
+    utilised: [
+      'React Native',
+      'Expo',
+      'React Navigation',
+      'Redux',
+      'NodeJS',
+      'Sequelize',
+      'Postgresql',
+      'Jest',
+    ],
+  },
+  {
+    title: 'Decagon Institute',
+    rolePlayed: '',
+    info:
+      "Decagon is one of Nigeria's institute for training software engineers",
+    utilised: [
+      'React Native',
+      'Expo',
+      'React Navigation',
+      'Redux',
+      'NodeJS',
+      'Sequelize',
+      'Postgresql',
+      'Jest',
+    ],
+  },
+];
