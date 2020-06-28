4b90d1eb7a32385d55768260e38edc3b8c25fa45
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-06-28 21:13:11 +0100

    ft: add carousel item

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-export default function index({ title, info }) {
+
+export default function index({ title, info, projectImages }) {
   return (
     <div className="project-card">
       <span className="project-card__title">{title}:</span> {info}
+      <div className="mt-4">
+        <Carousel showThumbs={false}>
+          {projectImages?.map((image, index) => (
+            <div className="project-card__carousel-item" key={index}>
+              <img src={image} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 }
