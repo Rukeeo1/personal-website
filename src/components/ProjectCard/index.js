e971f1b22371550b68e14d8af39be78decc3ad75
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-06-29 19:59:18 +0100

    chore: re-arrange utilized tools

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-
-export default function index({ title, info, projectImages }) {
+export default function index({ title, info, projectImages, utilizedTools }) {
   return (
-    <div className="project-card">
-      <span className="project-card__title">{title}:</span> {info}
-      <div className="mt-4">
+    <div className="project-card mt-4">
+      <span className="project-card__title">{title} :</span> {info}
+      <div className="mt-3">
         <Carousel showThumbs={false}>
           {projectImages?.map((image, index) => (
             <div className="project-card__carousel-item" key={index}>
@@ -16,6 +15,12 @@ export default function index({ title, info, projectImages }) {
           ))}
         </Carousel>
       </div>
+      <p>
+      <span className="mt-2" style={{ fontSize: '14px', fontWeight: 'bold' }}>
+        Tools :  
+      </span>
+        {' ' + utilizedTools}
+      </p>
     </div>
   );
 }
