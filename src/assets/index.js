42d2e63dae3b50a00c74f3a9d4c8794070ec2f66
Rukee Ojigbo <rukeeojigbo@gmail.com>
2020-11-09 17:11:59 +0100

    chore: import assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -12,6 +12,27 @@ import outworkztp from './outwork3.png';
 import outworktaskdes from './outwork4.png';
 import outworksummary from './outwork5.png';
 import outworkreports from './outwork6.png';
+import tme1 from './tme1.png';
+import tme2 from './tme2.png';
+import tme3 from './tme3.png';
+import s4me1 from './sf1.png';
+import s4me2 from './sf2.png';
+import s4me3 from './sf3.png';
+import s4me4 from './sf4.png';
+import s4me5 from './sf5.png';
+import learn1 from './l1.png';
+import learn2 from './l2.png';
+import learn3 from './l3.png';
+import learn4 from './l4.png';
+import learn5 from './l5.png';
+import learn6 from './l6.png';
+import did1 from './d1.png';
+import did2 from './d2.png';
+import did3 from './d3.png';
+import did4 from './d4.png';
+import did5 from './d5.png';
+import did6 from './d6.png';
+import did7 from './d7.png';
 
 export default {
   headerBckgrnd,
@@ -28,4 +49,25 @@ export default {
   outworktaskdes,
   outworksummary,
   outworkreports,
+  tme1,
+  tme2,
+  tme3,
+  s4me1,
+  s4me2,
+  s4me3,
+  s4me4,
+  s4me5,
+  learn1,
+  learn2,
+  learn3,
+  learn4,
+  learn5,
+  learn6,
+  did1,
+  did2,
+  did3,
+  did4,
+  did5,
+  did6,
+  did7,
 };
